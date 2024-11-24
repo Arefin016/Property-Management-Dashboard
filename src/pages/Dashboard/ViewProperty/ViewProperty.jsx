1a6a28bada8981710678a44e57378a1054822a9f
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-11-24 18:22:32 +0600

    Static Card has been added

--- src/pages/Dashboard/ViewProperty/ViewProperty.jsx
+++ src/pages/Dashboard/ViewProperty/ViewProperty.jsx
@@ -8,6 +8,61 @@ const ViewProperty = () => {
       >
         View All Property
       </h2>
+
+      <div
+        data-aos="flip-left"
+        data-aos-duration="1500"
+        className="grid md:grid-cols-3 grid-cols-1 gap-4 md:m-10 m-3"
+      >
+        {/* First Card */}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        {/* Second Card */}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        {/* Third Card */}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
